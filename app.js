26c4704ce7e518c31ff3ee307c0a3e3038ee34a5
Zak Porche <93185579+ZiMP45@users.noreply.github.com>
2023-08-19 23:43:03 +0000

    js for nav menu

---
+++ app.js
@@ -0,0 +1,9 @@
+// sidebar open and close
+
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+}
