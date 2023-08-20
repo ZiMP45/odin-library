849c04b95bfd8f4070e864ac1e980ee9c587f34b
Zak Porche <93185579+ZiMP45@users.noreply.github.com>
2023-08-20 01:04:37 +0000

    color change and transition

--- app.js
+++ app.js
@@ -1,9 +1,26 @@
-// sidebar open and close
+// sidebar open and close color change
+
+const card = document.querySelectorAll(".card");
+const container = document.getElementById("container");
 
 function openNav() {
-    document.getElementById("mySidenav").style.width = "250px";
+    document.getElementById("mySidenav").style.width = "300px";
+    container.style.marginLeft = "300px";
+    document.body.style.backgroundColor = 'var(--transition)';
+    document.querySelector(".top").style.backgroundColor = 'var(--darkest)';
+    document.querySelector(".bottom").style.backgroundColor = 'var(--darkest)';
+    card.forEach(card => {
+        card.style.border = '1px solid var(--dark)';
+    })
 }
   
 function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
+    container.style.marginLeft = "0";
+    document.body.style.backgroundColor = 'var(--lightest)'
+    document.querySelector(".top").style.backgroundColor = 'var(--darker)';
+    document.querySelector(".bottom").style.backgroundColor = 'var(--darker)';
+    card.forEach(card => {
+        card.style.border = '1px solid var(--light)';
+    })
 }
