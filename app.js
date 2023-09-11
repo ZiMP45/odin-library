77b16d24f7e6d268d606823ae034426e786b0934
Zak Porche <93185579+ZiMP45@users.noreply.github.com>
2023-09-11 02:32:14 +0000

    modal pop up and close

--- app.js
+++ app.js
@@ -1,3 +1,41 @@
+// add modal button functionality
+const openModalButtons = document.querySelectorAll('[data-modal-target]')
+const closeModalButtons = document.querySelectorAll('[data-close-button]')
+const overlay = document.getElementById('overlay')
+
+openModalButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        const modal = document.querySelector(button.dataset.modalTarget)
+        openModal(modal)
+    })
+})
+
+overlay.addEventListener('click', () => {
+    const modals = document.querySelectorAll('.modal.active')
+    modals.forEach(modal => {
+        closeModal(modal)
+    })
+})
+
+closeModalButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        const modal = button.closest('.modal')
+        closeModal(modal)
+    })
+})
+
+function openModal(modal) {
+    if (modal == null) return
+    modal.classList.add('active')
+    overlay.classList.add('active')
+}
+
+function closeModal(modal) {
+    if (modal == null) return
+    modal.classList.remove('active')
+    overlay.classList.remove('active')
+}
+
 // sidebar open and close color change
 const container = document.getElementById("container");
 
@@ -21,13 +59,4 @@ function closeNav() {
     card.forEach(card => {
         card.style.border = '1px solid var(--light)';
     })
-}
-
-// add button functionality
-function addNew() {
-    const cardsContainer = document.getElementsByClassName("cards");
-
-    const card = document.createElement("div");
-    card.innerHTML = ''
-
 }
